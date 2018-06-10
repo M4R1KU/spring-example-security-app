3c864000676a699d11ce821a33d0d842d8e43a74
Florian  Bürgi <florian-buergi@iet-gibb.ch>
2018-06-10 23:32:45 +0200

    add logging and rename CommandService Login not working properly

---
+++ src/main/kotlin/me/mkweb/gibb/m183/securityapp/service/SystemCommandService.kt
@@ -0,0 +1,29 @@
+package me.mkweb.gibb.m183.securityapp.service
+
+import org.apache.commons.text.StringEscapeUtils
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class SystemCommandService {
+    companion object {
+
+        val LOGGER = LoggerFactory.getLogger(SystemCommandService::class.java)
+
+        fun convertAndValidateParameters(arguments: String): Array<String> {
+            return arguments.split(" ").map { StringEscapeUtils.ESCAPE_XSI.translate(it) }.toTypedArray()
+        }
+    }
+
+    fun executeSecuredCommand(command: String, arguments: String): String {
+        val argumentArray = convertAndValidateParameters(arguments)
+        val processBuilder = ProcessBuilder(command, *argumentArray)
+        LOGGER.info("Executing system command: ${processBuilder.command()} with parameters: ${argumentArray.joinToString(" ")}")
+        val process = processBuilder.start()
+        val errorOutput = process.errorStream.bufferedReader().use { it.readText() }
+        LOGGER.warn(errorOutput)
+        return process.inputStream.bufferedReader().use { it.readText() }
+    }
+
+
+}
--- src/main/kotlin/me/mkweb/gibb/m183/securityapp/web/controller/CommandController.kt
+++ src/main/kotlin/me/mkweb/gibb/m183/securityapp/web/controller/CommandController.kt
@@ -1,14 +1,14 @@
 package me.mkweb.gibb.m183.securityapp.web.controller
 
-import me.mkweb.gibb.m183.securityapp.util.SystemCommandUtil
+import me.mkweb.gibb.m183.securityapp.service.SystemCommandService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
-
 @Controller
 @RequestMapping("/command")
-class CommandController {
+class CommandController @Autowired constructor(val systemCommandService: SystemCommandService) {
     @GetMapping()
     fun command(model: Model): Any {
         return "command"
@@ -16,7 +16,7 @@ class CommandController {
 
     @GetMapping("/execute")
     fun executeCommand( model: Model, @RequestParam("sysopt") sysopt: String): Any {
-        model.addAttribute("result", SystemCommandUtil.executeSecuredCommand("ipconfig", sysopt))
+        model.addAttribute("result", systemCommandService.executeSecuredCommand("date", sysopt))
         return "command"
     }
 }
